89d176f27270c8d86d100dc8035f31de37747ad9
OneOfOne <oneofone@gmail.com>
2016-03-24 12:37:25 +0200

    a bit of documentation

--- lfchan.go
+++ lfchan.go
@@ -9,6 +9,7 @@ import (
 
 const ptrSize = unsafe.Sizeof((*interface{})(nil))
 
+// Chan is a lock free channel that supports concurrent channel operations.
 type Chan struct {
 	q    []*interface{}
 	p    unsafe.Pointer
@@ -16,11 +17,16 @@ type Chan struct {
 	die  int32
 }
 
+// New returns a new channel with the buffer set to 1
 func New() *Chan {
 	return NewSize(1)
 }
 
+// New creates a buffered channel, with minimum length of 1
 func NewSize(sz int) *Chan {
+	if sz < 1 {
+		sz = 1
+	}
 	ch := &Chan{
 		q: make([]*interface{}, sz),
 	}
@@ -28,13 +34,14 @@ func NewSize(sz int) *Chan {
 	return ch
 }
 
-func (ch *Chan) Send(v interface{}) {
+// Send adds v to the buffer of the channel and returns true, if the channel is closed it returns false
+func (ch *Chan) Send(v interface{}) bool {
 	ln, idx := uintptr(len(ch.q))*ptrSize, uintptr(0)
 	for atomic.LoadInt32(&ch.die) == 0 {
 		p := unsafe.Pointer(uintptr(ch.p) + idx)
 		if atomic.CompareAndSwapPointer((*unsafe.Pointer)(atomic.LoadPointer(&p)), nil, unsafe.Pointer(&v)) {
 			atomic.AddInt32(&ch.size, 1)
-			return
+			return true
 		}
 		if idx += ptrSize; idx == ln {
 			time.Sleep(time.Millisecond)
@@ -42,15 +49,18 @@ func (ch *Chan) Send(v interface{}) {
 		}
 		runtime.Gosched()
 	}
+	return false
 }
 
-func (ch *Chan) Recv() interface{} {
+// Recv blocks until a value is available and returns v, true, or if the channel is closed and
+// the buffer is empty, it will return nil, false
+func (ch *Chan) Recv() (interface{}, bool) {
 	ln, idx := uintptr(len(ch.q))*ptrSize, uintptr(0)
 	for atomic.LoadInt32(&ch.die) == 0 || atomic.LoadInt32(&ch.size) > 0 {
 		p := unsafe.Pointer(uintptr(ch.p) + idx)
 		if v := atomic.SwapPointer((*unsafe.Pointer)(atomic.LoadPointer(&p)), nil); v != nil {
 			atomic.AddInt32(&ch.size, -1)
-			return *(*interface{})(v)
+			return *(*interface{})(v), true
 		}
 		if idx += ptrSize; idx == ln {
 			time.Sleep(time.Millisecond)
@@ -58,18 +68,10 @@ func (ch *Chan) Recv() interface{} {
 		}
 		runtime.Gosched()
 	}
-	return nil
+	return nil, false
 }
 
+//Close marks the channel as closed
 func (ch *Chan) Close() {
 	atomic.StoreInt32(&ch.die, 1)
-}
-
-type waiter uint64
-
-func (w *waiter) wait() {
-	if *w++; *w%10 == 0 {
-		time.Sleep(time.Millisecond)
-	}
-	runtime.Gosched()
 }
